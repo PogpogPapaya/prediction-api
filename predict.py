da0f2d15b9ec26e67ce1dfba264144a02406a525
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-08-26 11:07:34 +0700

    Finish predict.py

--- predict.py
+++ predict.py
@@ -1,22 +1,29 @@
 from keras.models import load_model
-my_model = load_model('./model-large.h5')
 import tensorflow as tf
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-classList = ['medium','ripe','unripe']
+import sys
+
+my_model = load_model('./model-large.h5')
+
+classList = ['medium', 'ripe', 'unripe']
 base_test = './test/'
-pictures = list(map(lambda x:base_test+x,os.listdir(base_test)))
-print(pictures)
-for i in pictures:
-  img = tf.keras.preprocessing.image.load_img(
-      i, target_size=(150, 150)
-  )
-  img_array = tf.keras.preprocessing.image.img_to_array(img)
-  img_array = img_array / 255
-  img_array = img_array.reshape(1,150,150,3)
-  predictions = my_model.predict(img_array)
-  print(predictions)
-#   imgplot = plt.imshow(img)
-#   plt.show()
-#   print(classList[np.argmax(predictions[0])])
+
+imgPath = sys.argv[1]
+
+img = tf.keras.preprocessing.image.load_img(
+    imgPath, target_size=(150, 150)
+)
+
+img_array = tf.keras.preprocessing.image.img_to_array(img)
+img_array = img_array / 255
+img_array = img_array.reshape(1, 150, 150, 3)
+predictions = my_model.predict(img_array)
+
+maxConfi = predictions[0][0]
+index = 0
+
+for i in range(3):
+    if maxConfi < predictions[0][i]:
+        maxConfi = predictions[0][i]
+        index = i
+
+print(classList[index], maxConfi)
